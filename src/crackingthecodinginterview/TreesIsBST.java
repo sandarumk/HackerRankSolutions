50287155e6e097ec49506788abbca1c53fe6c319
Dinu Kumarasiri <sandarumk@gmail.com>
2016-12-30 18:04:14 +0530

    resolved Compile Issue Is BST [Cracking Coding Interview] Solution

--- src/crackingthecodinginterview/TreesIsBST.java
+++ src/crackingthecodinginterview/TreesIsBST.java
@@ -12,33 +12,33 @@ public class TreesIsBST {
         boolean isBST;
         Node current = root;
         // root.data should be larger than the left subtree max value
-        if(current.left!= null && getMax(current.left) < current.data){
+        if (current.left != null && getMax(current.left) < current.data) {
             current = current.left;
             isBST = checkBST(current);
-            if(!isBST){
+            if (!isBST) {
                 return false;
             }
-        }else if (current.left!= null && getMax(current.left) >=current.data){
+        } else if (current.left != null && getMax(current.left) >= current.data) {
             return false;
         }
         current = root;
         // root.data should be smaller than the right subtree min value
-        if(current.right!= null && getMin(current.right) > current.data){
+        if (current.right != null && getMin(current.right) > current.data) {
             current = current.right;
-            isBST= checkBST(current);
-            if(!isBST){
+            isBST = checkBST(current);
+            if (!isBST) {
                 return false;
             }
-        }else if (current.right!= null && getMin(current.right) <= current.data){
+        } else if (current.right != null && getMin(current.right) <= current.data) {
             return false;
         }
         return true;
     }
 
-    int getMin(Node root){
-        int min  = Integer.MAX_VALUE;
-        while(root!= null){
-            if(root.data < min){
+    int getMin(Node root) {
+        int min = Integer.MAX_VALUE;
+        while (root != null) {
+            if (root.data < min) {
                 min = root.data;
             }
             root = root.left;
@@ -47,20 +47,23 @@ public class TreesIsBST {
         return min;
     }
 
-    int getMax(Node root){
-        int max  = Integer.MIN_VALUE;
-        while(root!= null){
-            if(root.data > max){
+    int getMax(Node root) {
+        int max = Integer.MIN_VALUE;
+        while (root != null) {
+            if (root.data > max) {
                 max = root.data;
             }
             root = root.right;
         }
         //System.out.println(max);
         return max;
+
     }
 
     class Node {
         int data;
         Node left;
         Node right;
+    }
+
 }
